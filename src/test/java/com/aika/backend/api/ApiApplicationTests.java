21c6a21e9e1662965f9ab1e47c80de4de240bcee
Thomas Couto (DESKTOP) <thomascouto@gmail.com>
2022-06-23 22:25:37 -0300

    test: app tests

---
+++ src/test/java/com/aika/backend/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aika.backend.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
