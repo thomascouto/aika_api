eacbe862ae054356d27226a64bcbd5bbad50004d
Thomas Couto (LAPTOP) <thomascouto@gmail.com>
2022-06-30 10:44:50 -0300

    feat: equipment model classes

--- src/main/java/com/aika/backend/api/equipmentmodel/EquipmentModel.java
+++ src/main/java/com/aika/backend/api/equipmentmodel/EquipmentModel.java
@@ -1,6 +1,5 @@
 package com.aika.backend.api.equipmentmodel;
 
-import java.util.UUID;
 import javax.persistence.Entity;
 import com.aika.backend.api.base.BaseClass;
 import lombok.Data;
@@ -12,12 +11,5 @@ import lombok.NoArgsConstructor;
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class EquipmentModel extends BaseClass {
-
-    public EquipmentModel(UUID id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
     private String name;
-
 }
